13c0a846294179ab88856aefbb8cf1177a4f95ef
ecalixt <ecalixt@clemson.edu>
2024-10-31 15:08:27 -0400

    UserAccount: added requirements and character limits for data involved in user account (i.e Email, FirstName, etc) and ensured email and user name are unique

--- TigerTix/Entities/UserAccount.cs
+++ TigerTix/Entities/UserAccount.cs
@@ -1,20 +1,36 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 
 namespace TigerTix.Entities
 {
+    // Ensure that user's email and username are unique 
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(UserName), IsUnique = true)]
     public class UserAccount
     {
-        
-        
+        [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(50, ErrorMessage = "Max 50 characters allowed.")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required.")]
+        [MaxLength(50, ErrorMessage = "Max 50 characters allowed.")]
         public string LastName { get; set; }
-        
+
+        [Required(ErrorMessage = "Email is required.")]
+        [MaxLength(100, ErrorMessage = "Max 100 characters allowed.")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Username is required.")]
+        [MaxLength(20, ErrorMessage = "Max 20 characters allowed.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MaxLength(20, ErrorMessage = "Max 20 characters allowed.")]
         public string Password { get; set; }
     }
 }
