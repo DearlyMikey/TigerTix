005300427ef9f6e7affd1340f2d4f513654d264b
ecalixt <ecalixt@clemson.edu>
2024-10-31 16:09:27 -0400

    TigerTixDbContext: File that is used to hold the Database of User Accounts

---
+++ TigerTix/Entities/TigerTixDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TigerTix.Entities
+{
+    public class TigerTixDbContext : DbContext
+    {
+        public TigerTixDbContext(DbContextOptions<TigerTixDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<UserAccount> UsersAccounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
