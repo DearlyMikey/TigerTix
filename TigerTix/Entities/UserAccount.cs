2542628c245da61303ac7d9e732edcd40bbbc9b4
ecalixt <ecalixt@clemson.edu>
2024-10-31 13:32:23 -0400

    Added an Entities folder that currently contains UserAccount

---
+++ TigerTix/Entities/UserAccount.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace TigerTix.Entities
+{
+    public class UserAccount
+    {
+        
+        
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
