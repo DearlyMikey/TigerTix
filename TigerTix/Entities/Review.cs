a4219f62210b5deb67a4d48c49bc62a3af08be3d
ecalixt <ecalixt@clemson.edu>
2024-12-08 23:24:32 -0500

    Review and ReviewViewModel Added

---
+++ TigerTix/Entities/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace TigerTix.Entities
+{
+    public class Review
+    {
+        public int Id { get; set; } // Primary Key
+        public string UserName { get; set; }
+        public string Comment { get; set; }
+        public int Rating { get; set; } // Rating from 1 to 5
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public int EventId { get; set; } // Foreign key to Product (assumed)
+    }
+}
---
+++ TigerTix/Models/ReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using TigerTix.Entities;
+
+namespace TigerTix.Models
+{
+    public class ReveiwModel
+    {
+        public class ReviewViewModel
+        {
+            public int Id { get; set; }
+            public string UserName { get; set; }
+            public string Comment { get; set; }
+            public int Rating { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public int EventId { get; set; }
+    }
+    }
+}
